3e001753f7eba2a98e0d219c1d08269d4cbeacef
FARCER <counter18@yandex.ru>
2018-04-11 22:35:51 +0300

    git init

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+"use strict";
+
+global.$ = {
+    path: {
+        task: require('./gulp/path/tasks.js')
+    },
+    gulp: require('gulp'),
+    browserSync: require('browser-sync').create(),
+    del: require('del')
+};
+
+$.path.task.forEach(function (taskPath) {
+    require(taskPath)();
+});
+
+$.gulp.task('dev', $.gulp.series(
+    'clean',
+    $.gulp.parallel(
+        'pug',
+        'fonts',
+        'styles:dev',
+        'img:dev',
+        'libsJS:dev',
+        'js:dev',
+        'svg'
+    )
+));
+
+$.gulp.task('build', $.gulp.series(
+    'clean',
+    $.gulp.parallel(
+        'pug',
+        'fonts',
+        'styles:build',
+        'styles:build-min',
+        'img:build',
+        'libsJS:build',
+        'js:build',
+        'js:build-min',
+        'svg'
+    )
+));
+$.gulp.task('default', $.gulp.series(
+    'dev',
+    $.gulp.parallel(
+        'watch',
+        'serve'
+    )
+));
